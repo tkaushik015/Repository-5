52ccdd4615fa4e61eac3480aed67d7bb534893f7
tkaushik015 <156272214+tkaushik015@users.noreply.github.com>
2024-05-02 18:35:59 -0700

    Update plot.py

--- pages/plot.py
+++ pages/plot.py
@@ -80,65 +80,65 @@ if st.sidebar.button('Submit'):
         fig = px.pie(runs_distribution_by_player, values='Runs', names='Name', title='Runs distribution by Player')
         st.plotly_chart(fig, use_container_width=True)
 
-        # Bar plot of Runs by Player
-        st.write("Bar plot of Runs by Player:")
+        # Bar plot of Bar Plot Showing Runs Scored by Players
+        st.write("Bar Plot Showing Runs Scored by Players:")
         runs_by_player = filtered_batting_df.groupby("Name")["Runs"].sum().reset_index()
-        fig = px.bar(runs_by_player, x='Name', y='Runs', title='Runs by Player')
+        fig = px.bar(runs_by_player, x='Name', y='Runs', title='Bar Plot Showing Runs Scored by Players')
         st.plotly_chart(fig, use_container_width=True)
 
         # Pie chart of Hundred's distribution by Country
-        st.write("Pie chart of Hundred's distribution by Country:")
+        st.write("# Hundred's distribution by Country:")
         hundreds_by_country = batting_df.groupby("Country")["Hundreds"].sum().reset_index()
         fig = px.pie(hundreds_by_country, values='Hundreds', names='Country', title='Hundred\'s distribution by Country')
         st.plotly_chart(fig, use_container_width=True)
 
         # Pie chart of Fifties distribution by Country for all countries
-        st.write("Pie chart of Fifties distribution by Country:")
+        st.write("# Fifties distribution by Country:")
         fifties_by_country_all = batting_df.groupby("Country")["Fifties"].sum().reset_index()
         fig = px.pie(fifties_by_country_all, values='Fifties', names='Country', title='Fifties distribution by Country')
         st.plotly_chart(fig, use_container_width=True)
 
         # Bar plot of Batting Averages by Player
-        st.write("Bar plot of Batting Averages by Player:")
-        fig_avg = px.bar(filtered_batting_df, x='Name', y='Average', title='Batting Averages by Player')
+        st.write("# Batting Averages of Players:")
+        fig_avg = px.bar(filtered_batting_df, x='Name', y='Average', title='Batting Averages of Players')
         st.plotly_chart(fig_avg, use_container_width=True)
 
         # Best Batting Average table
-        st.subheader('Best Batting Average')
+        st.subheader('**Best Batting Average**')
         best_batting_average = filtered_batting_df[['Name', 'Average']].copy()
         best_batting_average = best_batting_average.sort_values(by='Average', ascending=False).reset_index(drop=True)
         best_batting_average.index += 1  # Start numbering from 1
         st.write(best_batting_average)
 
         # Table of Top Power hitters
-        st.subheader('Top Power hitters')
+        st.subheader('**Top Power hitters**')
         top_power_hitters = filtered_batting_df[filtered_batting_df['Strike_rate'] >= 150].reset_index(drop=True)
         top_power_hitters.index += 1  # Start numbering from 1
         st.write(top_power_hitters)
 
         # Table of Number Of Fifties By Each Player
-        st.subheader('Number Of Fifties By Each Player')
+        st.subheader('**Number Of Fifties By Each Player**')
         fifties_by_player = filtered_batting_df[['Name', 'Fifties']].copy()
         fifties_by_player = fifties_by_player[fifties_by_player['Fifties'] != 0]  # Filter non-zero fifties
         fifties_by_player.index += 1  # Start numbering from 1
         st.write(fifties_by_player)
 
         # Table of Number Of Hundreds By Each Player
-        st.subheader('Number Of Hundreds By Each Player')
+        st.subheader('**Number Of Hundreds By Each Player**')
         hundreds_by_player = filtered_batting_df[['Name', 'Hundreds']].copy()
         hundreds_by_player = hundreds_by_player[hundreds_by_player['Hundreds'] != 0]  # Filter non-zero hundreds
         hundreds_by_player.index += 1  # Start numbering from 1
         st.write(hundreds_by_player)
 
         # Table of Most Boundaries
-        st.subheader('Most Boundaries')
+        st.subheader('**Most Boundaries**')
         most_boundaries = filtered_batting_df[['Name', 'Fours']].copy()
         most_boundaries = most_boundaries[most_boundaries['Fours'] != 0]  # Filter non-zero boundaries
         most_boundaries.index += 1  # Start numbering from 1
         st.write(most_boundaries)
 
         # Table of Most Sixes
-        st.subheader('Most Sixes')
+        st.subheader('**Most Sixes**')
         most_sixes = filtered_batting_df[['Name', 'Sixes']].copy()
         most_sixes = most_sixes[most_sixes['Sixes'] != 0]  # Filter non-zero sixes
         most_sixes.index += 1  # Start numbering from 1
